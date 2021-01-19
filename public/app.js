79f14cf25f0c8f70e9d1be9e2541e042d75b5826
Muhammad zubait <zubairsaleem1210@gmail.com>
2021-01-19 17:54:59 +0500

    LEARNING POINT

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
-const url = 'http://localhost:5000';
+// const url = 'http://localhost:5000'
+const url = 'https://chatapp-zubair.herokuapp.com'
 var socket = io(url);
 socket.on('connect', function () {
     console.log("connected")
