390008f31d975587af3ddacfdc8960a15350febb
Muhammad zubait <zubairsaleem1210@gmail.com>
2021-01-20 18:24:32 +0500

    learning point

--- core/index.js
+++ core/index.js
@@ -1,4 +1,5 @@
 module.exports = {
 
-    SERVER_SECRET: process.env.SECRET || "1234"
+    SERVER_SECRET: process.env.SECRET || "1234",
+    MONGOOSE_DBURI: process.env.MONGOOSE_DBURI
 }
--- dbrepo/modles.js
+++ dbrepo/modles.js
@@ -1,11 +1,10 @@
+var { MONGOOSE_DBURI } = require("../core/index")
 var mongoose = require('mongoose');
 
+let dbURI =  MONGOOSE_DBURI
 
 
-
-// let dbURI = "mongodb+srv://zubairabc:zubairabc@cluster0.9qvbs.mongodb.net/testdatabase";
-// let dbURI = 'mongodb://localhost:27017/abc-database';
-let dbURI = "mongodb+srv://zubairabc:zubairabc@cluster0.j83vk.mongodb.net/testdatabase?retryWrites=true&w=majority"
+// let dbURI = "mongodb+srv://zubairabc:zubairabc@cluster0.j83vk.mongodb.net/testdatabase?retryWrites=true&w=majority"
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
